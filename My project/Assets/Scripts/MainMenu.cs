e3b7ad2f3849240555ffa61414240001698a1a87
AnaEValle <emiliavp30@gmail.com>
2024-08-10 18:08:46 -0600

    Quit Game
    Quit game functional

--- My project/Assets/Scripts/MainMenu.cs
+++ My project/Assets/Scripts/MainMenu.cs
@@ -8,6 +8,13 @@ public class MainMenu : MonoBehaviour
    public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
 
     }
+
 }
